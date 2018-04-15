b82efce328d7538b2c02afd54940579bd03e986b
Grefrut <artur.bunko@gmail.com>
2018-04-15 14:10:38 +0300

    Sign in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
     def new 
-        User = User.new
+        @user = User.new
     end
 
     def create
-      User = User.new(users_params)
+      @user = User.new(users_params)
       if @user.save
         redirect_to root_path
       else
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
     private
         def users_params
-            params.require(:users).permit(:first_name,
+            params.require(:user).permit(:first_name,
                                           :last_name,
                                           :email,
                                           :password,
