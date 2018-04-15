e44300b858986c650aa55dca0d5346dbb61f5b7b
Grefrut <artur.bunko@gmail.com>
2018-04-15 13:51:51 +0300

    Controller Users and UserSessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    def new 
+        User = User.new
+    end
+
+    def create
+      User = User.new(users_params)
+      if @user.save
+        redirect_to root_path
+      else
+        render :new
+      end
+    end
+
+    private
+        def users_params
+            params.require(:users).permit(:first_name,
+                                          :last_name,
+                                          :email,
+                                          :password,
+                                          :password_confirmation)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'user_sessions/create'
+
+  get 'user_sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
