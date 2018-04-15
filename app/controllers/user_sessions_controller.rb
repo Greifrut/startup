59df871367214e8ecc0585d40f5caa56635e5b85
Grefrut <artur.bunko@gmail.com>
2018-04-15 14:33:18 +0300

    fixed

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -18,6 +18,7 @@ class UserSessionsController < ApplicationController
   end
 
   private
+
     def user_session_params
       params.require(:user_session).permit(:email, :password, :remember_me)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,4 @@ Rails.application.routes.draw do
 
   delete '/sign_out', to: 'user_sessionss#destroy', as: :sign_out
   get '/sign_in', to: 'user_sessions#new', as: :sign_in
-
 end
