89fac03e0361fd53f52384af79b7540abdccaec6
Grefrut <artur.bunko@gmail.com>
2018-04-15 13:58:12 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'user_sessions/create'
+  resources :users, only: [:new, :create]
+  resources :user_sessions, only: [:create, :destroy]
 
-  get 'user_sessions/destroy'
+  delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
+  get '/sign_in', to: 'user_session#new', as: :sign_in
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
