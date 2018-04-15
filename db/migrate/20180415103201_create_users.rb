cea556cb34de7240431af0fc6546447580779cc2
Grefrut <artur.bunko@gmail.com>
2018-04-15 13:41:30 +0300

    User model

---
+++ db/migrate/20180415103201_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :crypted_password
+      t.string :password_salt
+      t.string :persistence_token
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
