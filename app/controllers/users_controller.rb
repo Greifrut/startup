2a9fbf7aabaff7556a1b29caf5d0d2ccdde76829
Greifrut <artur.bunko@gmail.com>
2018-04-21 17:02:22 +0300

    user style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(users_params)
     if @user.save
-      redirect_to root_path
+      redirect_to current_user
     else
       render :new
     end
