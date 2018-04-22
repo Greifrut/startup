7232c5b12aad5eaf1aeb9fe5dd10c163f6fdeef4
Greifrut <artur.bunko@gmail.com>
2018-04-22 15:21:18 +0300

    user#show  style

--- Gemfile
+++ Gemfile
@@ -28,5 +28,5 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'guard-livereload', '~> 2.5', require: false
 end
-
+gem 'font-awesome-sass'
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :users
   resources :user_sessions, only: [:create, :destroy]
 
-  delete '/sign_out', to: 'user_sessionss#destroy', as: :sign_out
+  delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
   get '/sign_in', to: 'user_sessions#new', as: :sign_in
 end
