cfbf0238620b3baecb21c6a22b4b400878a51930
Greifrut <artur.bunko@gmail.com>
2018-04-23 14:54:13 +0300

    action_name test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,13 @@ class UsersController < ApplicationController
       @user = User.new
   end
 
-  def show; end
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 
   def index
     @user = User.all
@@ -17,6 +23,16 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(users_params)
+      redirect_to current_user
+    else
+      render 'edit'
+    end
+  end
+
 
   private
     def users_params
